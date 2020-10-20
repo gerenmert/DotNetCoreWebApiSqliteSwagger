abb3d100aa059eb971b0b3efb4d2af1f8c02390e
mertgeren <mert.geren@iqb.solutions>
2020-10-20 15:41:33 +0300

    user model class added

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DotNetCoreWebApiSqliteSwagger.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+    }
+}
