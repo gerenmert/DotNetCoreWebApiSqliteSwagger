3101ec4fb7dea1aaf00eabd7cb09424f8c1a7700
mertgeren <mert.geren@iqb.solutions>
2020-10-20 15:55:17 +0300

    dbcontext class added

---
+++ Models/UserContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetCoreWebApiSqliteSwagger.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext (DbContextOptions<UserContext> options) : base (options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
